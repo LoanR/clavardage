97b8abad4bae31a95027e0891542a0af35944810
LoanR <loan.rbrt@gmail.com>
2019-04-20 14:05:44 +0200

    References #5 | API post route and persistance

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 // import express from 'express';
 const express = require('express');
-// import cookieSession from 'cookie-session';
-// import { urlencoded } from 'body-parser';
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const ent = require('ent');
 
-// const urlencodedParser = urlencoded({ extended: false });
+const urlencodedParser = bodyParser.json();
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
-
-// server.listen(80);
-
-// app.use(cookieSession({
-//   name: 'session',
-//   keys: ['username'],
-// }));
 
 const MESSAGE = 0;
 const DATE = 1;
@@ -25,7 +19,7 @@ const entryTypes = {
   connection: CONNECTION,
 };
 
-let chatRooms = [
+const chatRooms = [
   {
     name: 'seed test',
     url: 'seedtest',
@@ -73,7 +67,7 @@ let chatRooms = [
       {
         type: MESSAGE,
         timestamp: new Date(Date.UTC(2016, 11, 21, 3, 16, 34)),
-        content: 'message in another room',
+        content: ent.encode('éléphant <strong> énervé </strong>'),
         author: 'mister',
       },
       {
@@ -92,9 +86,49 @@ let chatRooms = [
   },
 ];
 
+function sendNewEntry(currentRoom, newEntry, res) {
+  currentRoom.entries.push(newEntry);
+  res.sendStatus(200);
+  let newEntryRender = null;
+  ejs.renderFile(
+    './views/partials/roomDetail/entry.ejs',
+    {
+      entry: newEntry,
+      entryTypes,
+    },
+    (err, result) => {
+      if (err) {
+        // eslint-disable-next-line no-alert
+        alert(err);
+      } else {
+        newEntryRender = result;
+      }
+    },
+  );
+  io.emit(
+    'newEntry',
+    newEntryRender,
+  );
+}
+
+app.use((req, ignore, next) => {
+  req.io = io;
+  next();
+});
+
+io.on('connection', (socket) => {
+  socket.emit('newUser');
+});
+
 app.get('/', (req, res) => {
-  res.render('pages/index.ejs', { pageTitle: 'Clavardage', chatRoom: null, chatRooms });
-}).get('/:roomName', (req, res) => {
+  res.render('pages/index.ejs', {
+    pageTitle: 'Clavardage',
+    chatRoom: null,
+    chatRooms,
+  });
+});
+
+app.get('/:roomName', (req, res) => {
   const currentRoom = chatRooms.find(room => room.url === req.params.roomName);
   if (!currentRoom) {
     res.redirect('/');
@@ -102,17 +136,41 @@ app.get('/', (req, res) => {
     res.render('pages/index.ejs', {
       pageTitle: `Clavardage ${currentRoom.name}`,
       chatRoom: currentRoom.name,
+      machin: currentRoom.url,
       entries: currentRoom.entries,
       entryTypes,
     });
   }
-}).use((ignore, res) => {
-  res.setHeader('Content-Type', 'test/plain');
-  res.status(404).send('Page not found!');
 });
 
-io.on('connection', (socket) => {
-  socket.emit('newUser');
+app.post('/messages', urlencodedParser, (req, res) => {
+  const errors = [];
+  if (!req.body || typeof req.body.author === 'undefined' || req.body.author === '') {
+    errors.push('You should have a username...');
+  }
+  if (!req.body || typeof req.body.content === 'undefined' || req.body.content === '') {
+    errors.push('The message cannot be empty.');
+  }
+  const currentRoom = chatRooms.find(room => room.url === req.body.roomUrl);
+  if (!req.body || typeof req.body.roomUrl === 'undefined' || !currentRoom) {
+    errors.push('You cannot send a message in another room.');
+  }
+  if (errors.length > 0) {
+    res.status(400).send({ errors });
+  } else {
+    const newEntry = {
+      type: MESSAGE,
+      timestamp: new Date(Date.now()),
+      content: ent.encode(req.body.content),
+      author: ent.encode(req.body.author),
+    };
+    sendNewEntry(currentRoom, newEntry, res);
+  }
+});
+
+app.use((ignore, res) => {
+  res.setHeader('Content-Type', 'test/plain');
+  res.status(404).send('Page not found!');
 });
 
 server.listen(8080);
